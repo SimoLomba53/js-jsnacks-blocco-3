bdfd392247cf6013f023149820ae9bb328ed3328
SimoLomba53 <simolomba253@gmail.com>
2023-01-30 14:28:15 +0100

    snack 2 ancora con problemi

--- snack 2/main2.js
+++ snack 2/main2.js
@@ -5,16 +5,20 @@ arrayVuoto = [
 
 ]
 
-let i = 0
-let somma;
+//let i = 0
+//let somma;
+let somma = somma + arrayVuoto[i];
+
 
 while (i < 50) {
-    arrayVuoto = prompt("Che numero devo aggiungere?");
+    arrayVuoto = parseint(prompt("Che numero devo aggiungere?"));
     console.log = arrayVuoto;
-    somma = somma + arrayVuoto[i];
-    console.log(somma);
+    //somma = somma + arrayVuoto[i];
+    //console.log(somma);
     i++;
 }
+
+console.log(somma);
 
 
 
