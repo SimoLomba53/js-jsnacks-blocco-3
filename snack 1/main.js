674895b8de28d69d9089115c031018da3af045ed
SimoLomba53 <simolomba253@gmail.com>
2023-01-30 16:11:54 +0100

    snack 1 completo

--- snack 1/main.js
+++ snack 1/main.js
@@ -8,16 +8,16 @@ const maxValueRange = prompt("Qual'è il valore massimo?");
 console.log(maxValueRange);
 
 
-let i = 0;
-const randomNumber = (Math.random());
-console.log(randomNumber);
 
-while (randomNumber > minValueRange && randomNumber < maxValueRange) {
-    //const randomNumber = (Math.random())[i];
-    //console.log(randomNumber);
-    const numbergenerate = randomNumber;
-    console.log(numbergenerate);
-    i++;
+let randomNumber = 20;
+
+while (randomNumber < minValueRange && randomNumber > maxValueRange) {
+    randomNumber = (Math.random());
+
+
+
 }
 
+
+console.log(randomNumber);
 
