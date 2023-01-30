1bafd04e86f23ca3d59bcb15223144ef4f6d232a
SimoLomba53 <simolomba253@gmail.com>
2023-01-30 13:15:05 +0100

    snack 1 non finito

---
+++ snack 1/main.js
@@ -0,0 +1,19 @@
+//OTTIENI NUMERO CASUALE IN UN RANGE SPECIFICATO
+//CHIEDI A UTENTE VALORE MINIMO E MASSIMO IN CUI GENERARE IL NUMERO
+
+const minValueRange = prompt("Qual'è il valore minimo?");
+console.log(minValueRange);
+
+const maxValueRange = prompt("Qual'è il valore massimo?");
+console.log(maxValueRange);
+
+//const randomNumber = Math.floor(Math.random() * 10);
+
+let = 0;
+
+while (i < minValueRange && i > maxValueRange) {
+    const randomNumber = Math.floor(Math.random() * maxValueRange)[i];
+    console.log = randomNumber;
+    i++;
+}
+
