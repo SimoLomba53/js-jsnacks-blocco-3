5e6eb43bf7f5c81ead1cafdc3a364421efe1931d
SimoLomba53 <simolomba253@gmail.com>
2023-01-30 14:17:08 +0100

    snack 2 errato

---
+++ snack 2/main2.js
@@ -0,0 +1,19 @@
+//CREA UN ARRAY VUOTO
+//CHIEDI ALL'UTENTE I NUMERI DA INSERIRE NELL'ARRAY FINO A QUANDO LA SOMMA DI ESSI E' MINORE DI 50
+
+arrayVuoto = [
+
+]
+
+let i = 0
+let somma;
+
+while (i < 50) {
+    arrayVuoto = prompt("Che numero devo aggiungere?")[i];
+    console.log = arrayVuoto;
+    somma = somma + arrayVuoto;
+    i++;
+}
+
+console.log(somma);
+
